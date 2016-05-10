c0803ad7852e7690e04e2701cca2eef72c15b40d
FahimChowdhury <fshahriar89@yahoo.com>
2016-05-10 10:39:24 +0600

    Add files via upload

---
+++ EnemyShipTesting/src/EnemyShipTesting.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class EnemyShipTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EnemyShipFactory shipFactory= new EnemyShipFactory();
+		EnemyShip theEnemy= null;
+		Scanner userInput=new Scanner(System.in);
+		System.out.println("What type ship? (U/ R/ B)");
+		if(userInput.hasNextLine())
+		{
+			String typeOfShip= userInput.nextLine();
+			theEnemy=shipFactory.makeEnemyShip(typeOfShip);
+		}
+		
+		if(theEnemy!=null)
+		{
+			doStuffEnemy(theEnemy);
+		}
+		else
+			System.out.println("Enter a U, R or B next time");
+		
+	}
+	
+	public static void doStuffEnemy(EnemyShip anEnemyShip)
+	{
+		anEnemyShip.displayEnemyShip();
+		anEnemyShip.followHeroShip();
+		anEnemyShip.enemyShipShoots();
+	}
+
+}
